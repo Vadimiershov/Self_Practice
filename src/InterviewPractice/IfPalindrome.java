7a6aee1d243b915b783880f5d6b426495fa7f287
Vadimiershov <40811461+Vadimiershov@users.noreply.github.com>
2021-04-26 21:03:48 -0500

    Palindrome added

---
+++ src/InterviewPractice/IfPalindrome.java
@@ -0,0 +1,16 @@
+package InterviewPractice;
+
+import java.util.*;
+
+public class IfPalindrome {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+}
---
+++ src/InterviewPractice/SumOfAllDigitFromTheSting.java
@@ -0,0 +1,33 @@
+package InterviewPractice;
+
+public class SumOfAllDigitFromTheSting {
+
+    public static void main(String[] args) {
+
+        String str = "asd34df7";
+        int sum = 0;
+        String num = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if(Character.isDigit(str.charAt(i))) {
+                num += str.charAt(i);
+
+            if(i==str.length()-1 || !Character.isDigit(str.charAt(i+1))) {
+                sum += Integer.parseInt(num);
+                num = "";
+
+            }
+            }
+
+
+
+
+        }
+
+
+
+        System.out.println(sum);
+    }
+
+
+}
